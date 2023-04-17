49752200c2b4a6fc64db5ded535818a418e3cb09
Rin Arakaki <rnarkkx@gmail.com>
2023-04-18 03:03:31 +0900

    Update examples.rs

--- chapter1/rust/examples.rs
+++ chapter1/rust/examples.rs
@@ -1,1 +1,8 @@
+use arrow;
 
+fn first_example() {
+  let data = Vec<i64>![1, 2, 3, 4];
+  let arr = std::make_shared<arrow::Int64Array>(data.len(),
+                                                 arrow::Buffer::Wrap(data));
+  println!("{}", arr.to_string();
+}
