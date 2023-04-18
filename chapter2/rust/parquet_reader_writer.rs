0741fd272fdc7b14e6d00bbfb55d0e152383d5aa
Rin Arakaki <rnarkkx@gmail.com>
2023-04-18 14:21:46 +0900

    Update parquet_reader_writer.rs

--- chapter2/rust/parquet_reader_writer.rs
+++ chapter2/rust/parquet_reader_writer.rs
@@ -1,25 +1,22 @@
-#include <arrow/table.h>
-#include <parquet/arrow/reader.h>
-#include <parquet/arrow/writer.h>
-
 use core::fs::File;
 use arrow;
-use parquet::file::reader::{FileReader, SerializedFileReader};
+use parquet::file::{
+    reader::{FileReader, SerializedFileReader},
+    writer::SerializedFileWriter
+};
 
 fn main() {
-    let input = File::open("../../sample_data/train.parquet").unwrap();
-    std::unique_ptr<parquet::arrow::FileReader> arrow_reader;
-    let status = parquet::arrow::OpenFile(input, arrow::default_memory_pool(),
-                                         &arrow_reader);
+    let file = File::open("../../sample_data/train.parquet").unwrap();
+    let reader = SerializedFileReader::new(file).unwrap();
 
     std::shared_ptr<arrow::Table> table;
-    PARQUET_THROW_NOT_OK(arrow_reader.ReadTable(&table));
-
+    PARQUET_THROW_NOT_OK(reader.ReadTable(&table));
     println!("{}", table.ToString());
 
-    let outfile = arrow::io::FileOutputStream::Open("train.parquet").unwrap();
+    let outfile = File::create("train.parquet").unwrap();
+    let writer = SerializedFileWriter::new(outfile).unwrap();
     let i64 chunk_size = 1024;
-  PARQUET_THROW_NOT_OK(parquet::arrow::WriteTable(
-      *table, arrow::default_memory_pool(), outfile, chunk_size));
-  PARQUET_THROW_NOT_OK(outfile.Close());
+    PARQUET_THROW_NOT_OK(parquet::arrow::WriteTable(
+          *table, outfile, chunk_size));
+    writer.close().unwrap();
 }
