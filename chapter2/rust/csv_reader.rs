d7207e19898830a1d5a0c53d2b67b6cf0076d88a
Rin Arakaki <rnarkkx@gmail.com>
2023-04-18 12:29:57 +0900

    Update csv_reader.rs

--- chapter2/rust/csv_reader.rs
+++ chapter2/rust/csv_reader.rs
@@ -1,5 +1,5 @@
 use arrow::{
-    csv::api,  // the csv functions and objects
+    csv::Reader,  // the csv functions and objects
     io::api,   // for opening the file
     table   // to read the data into a table
 };
@@ -14,30 +14,30 @@ fn main() {
         return 1;
     }
 
-  std::shared_ptr<arrow::io::InputStream> input = *maybe_input;
-  auto io_context = arrow::io::default_io_context();
-  auto read_options = arrow::csv::ReadOptions::Defaults();
-  auto parse_options = arrow::csv::ParseOptions::Defaults();
-  auto convert_options = arrow::csv::ConvertOptions::Defaults();
+    std::shared_ptr<arrow::io::InputStream> input = *maybe_input;
+    let io_context = arrow::io::default_io_context();
+    let read_options = arrow::csv::ReadOptions::Defaults();
+    let parse_options = arrow::csv::ParseOptions::Defaults();
+    let convert_options = arrow::csv::ConvertOptions::Defaults();
 
-  auto maybe_reader = arrow::csv::TableReader::Make(
-      io_context, input, read_options, parse_options, convert_options);
+    let maybe_reader = arrow::csv::TableReader::Make(
+        io_context, input, read_options, parse_options, convert_options);
 
-  if (!maybe_reader.ok()) {
-    // handle any instantiation errors
-    std::cerr << maybe_reader.status().message() << std::endl;
-    return 1;
-  }
+    if !maybe_reader.ok() {
+        // handle any instantiation errors
+        std::cerr << maybe_reader.status().message() << std::endl;
+        return 1;
+    }
 
-  std::shared_ptr<arrow::csv::TableReader> reader = *maybe_reader;
-  // finally read the data from the file
-  auto maybe_table = reader->Read();
-  if (!maybe_table.ok()) {
-    // handle any errors such as CSV syntax errors
-    // or failed type conversion errors, etc.
-    std::cerr << maybe_table.status().message() << std::endl;
-    return 1;
-  }
+    std::shared_ptr<arrow::csv::TableReader> reader = *maybe_reader;
+    // finally read the data from the file
+    let maybe_table = reader->Read();
+    if !maybe_table.ok() {
+        // handle any errors such as CSV syntax errors
+        // or failed type conversion errors, etc.
+        std::cerr << maybe_table.status().message() << std::endl;
+        return 1;
+    }
 
   std::shared_ptr<arrow::Table> table = *maybe_table;
   std::cout << table->ToString() << std::endl;
