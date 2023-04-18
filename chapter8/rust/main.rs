629ca7de9d980de24fbbd8df97a8cacfc09dd6a7
root <rnarkkx@gmail.com>
2023-04-18 10:49:25 +0000

    Update

--- chapter8/rust/main.rs
+++ chapter8/rust/main.rs
@@ -1,7 +1,7 @@
+/*
 import (
 	"github.com/PacktPublishing/In-Memory-Analytics-with-Apache-Arrow-/utils"
 	"github.com/apache/arrow/go/v8/arrow/arrio"
-	"github.com/apache/arrow/go/v8/arrow/flight"
 	"github.com/apache/arrow/go/v8/arrow/ipc"
 	"github.com/apache/arrow/go/v8/arrow/memory"
 	"github.com/apache/arrow/go/v8/parquet/file"
@@ -10,161 +10,126 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
+*/
 
 use arrow::{
-    flight,
+    flight::{Criteria, FlightInfo, Ticket, FlightDescriptor, FlightEndpoint},
     ipc
 };
 use parquet;
+use aws_config::;
+use aws_sdk_s3::Client as S3Client;
 
 struct Server {
-	flight.BaseFlightServer,
-	s3_client: *s3.Client,
-	bucket: Sring
+	// flight.BaseFlightServer,
+	s3_client: S3Client,
+	bucket: String
 }
 
 impl Server {
-    fn new() -> Self {
-        Server{
-            s3_client: s3.New(s3.Options{Region: "us-east-2"}),
-            bucket:   "ursa-labs-taxi-data",
+    pub fn new() -> Self {
+        Self {
+            s3_client: S3Client::new(s3::Options { region: "us-east-2" }),
+            bucket: "ursa-labs-taxi-data".to_owned(),
         }
     }
 
-    fn list_flights(c *flight.Criteria, fs flight.FlightService_ListFlightsServer) error {
-        var prefix string
-        if len(c.Expression) > 0 {
-            prefix = string(c.Expression)
+    pub async fn list_flights(&self, c: Criteria, fs: flight.FlightService_ListFlightsServer) -> error {
+        let mut prefix = String;
+        if c.expression.len() > 0 {
+            prefix = c.expression.to_string()
         }
-        list, err := s.s3Client.ListObjectsV2(fs.Context(),
-            &s3.ListObjectsV2Input{
-                Bucket: &s.bucket,
-                Prefix: &prefix,
-            })
-        if err != nil {
-            return err
-        }
+        let list = self.s3_client.list_objects_v2()
+            .bucket(&self.bucket)
+            .prefix(&prefix)
+            .send().await
+            .unwrap();
     
-        for _, f := range list.Contents {
-            if !strings.HasSuffix(*f.Key, ".parquet") {
+        for content in list.contents().unwrap() {
+            let key = content.key().unwrap();
+            if !key.ends_with(".parquet") {
                 continue
             }
-            info, err := s.getFlightInfo(fs.Context(), *f.Key, f.Size)
-            if err != nil {
-                return err
-            }
-            if err := fs.Send(info); err != nil {
-                return err
-            }
+            let info = self.get_flight_info(fs.Context(), key, content.size()).unwrap();
+            fs.Send(info).unwrap();
     
         }
     
-        return nil
+        Ok(())
+    }
+
+    fn get_flight_info(ctx: context.Context, key: &str, size: i64)
+        -> Result<FlightInfo>
+    {
+        let s3file = utils.NewS3File(ctx, self.s3_client,
+            self.bucket, key, size).unwrap();
+    
+        let pr = file.NewParquetReader(s3file).unwrap();
+        // defer pr.Close()
+    
+        let sc = pqarrow.FromParquet(pr.MetaData().Schema, nil, nil).unwrap();
+    
+        Ok(FlightInfo {
+            schema: flight.SerializeSchema(sc, memory.DefaultAllocator),
+            flight_descriptor: &FlightDescriptor {
+                r#type: flight.DescriptorPATH,
+                path: vec![key],
+            },
+            endpoint: vec![FlightEndpoint {{
+                ticket: Some(Ticket { ticket: []byte(key) }),
+                location: Vec::new()
+            }}],
+            total_records: pr.NumRows(),
+            total_bytes: -1,
+        })
+    }
+
+    pub fn do_get(&self, tkt: Ticket, fs: flight.FlightService_DoGetServer) error {
+        let path = string(tkt.ticket)
+        let sf = utils.NewS3File(fs.Context(), self.s3_client,
+            self.bucket, path, utils.UnknownSize).unwrap();
+        let pr = file.NewParquetReader(sf).unwrap();
+        defer pr.Close()
+        let arrowRdr = pqarrow.NewFileReader(pr,
+            pqarrow.ArrowReadProperties { parallel: true, batch_size: 100000 },
+            memory.DefaultAllocator).unwrap();
+        let rr = arrowRdr.GetRecordReader(fs.Context(), nil, nil).unwrap();
+        // defer rr.Release()
+        let wr = flight.NewRecordWriter(fs, ipc.WithSchema(rr.Schema()));
+        // defer wr.Close()
+        let n, err = arrio.Copy(wr, rr);
+        println!("wrote", n, "record batches");
+    
+        err
     }
 }
 
 
-
-func (s *server) getFlightInfo(ctx context.Context, key string, filesize int64) (*flight.FlightInfo, error) {
-	s3file, err := utils.NewS3File(ctx, s.s3Client,
-		s.bucket, key, filesize)
-	if err != nil {
-		return nil, err
-	}
-
-	pr, err := file.NewParquetReader(s3file)
-	if err != nil {
-		return nil, err
-	}
-	defer pr.Close()
-
-	sc, err := pqarrow.FromParquet(pr.MetaData().Schema, nil, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	return &flight.FlightInfo{
-		Schema: flight.SerializeSchema(sc, memory.DefaultAllocator),
-		FlightDescriptor: &flight.FlightDescriptor{
-			Type: flight.DescriptorPATH,
-			Path: []string{key},
-		},
-		Endpoint: []*flight.FlightEndpoint{{
-			Ticket: &flight.Ticket{Ticket: []byte(key)},
-		}},
-		TotalRecords: pr.NumRows(),
-		TotalBytes:   -1,
-	}, nil
-}
-
-func (s *server) DoGet(tkt *flight.Ticket, fs flight.FlightService_DoGetServer) error {
-	path := string(tkt.Ticket)
-
-	sf, err := utils.NewS3File(fs.Context(), s.s3Client,
-		s.bucket, path, utils.UnknownSize)
-	if err != nil {
-		return err
-	}
-
-	pr, err := file.NewParquetReader(sf)
-	if err != nil {
-		return err
-	}
-	defer pr.Close()
-
-	arrowRdr, err := pqarrow.NewFileReader(pr,
-		pqarrow.ArrowReadProperties{
-			Parallel: true, BatchSize: 100000,
-		}, memory.DefaultAllocator)
-	if err != nil {
-		return err
-	}
-	rr, err := arrowRdr.GetRecordReader(fs.Context(), nil, nil)
-	if err != nil {
-		return err
-	}
-	defer rr.Release()
-
-	wr := flight.NewRecordWriter(fs, ipc.WithSchema(rr.Schema()))
-	defer wr.Close()
-
-	n, err := arrio.Copy(wr, rr)
-	fmt.Println("wrote", n, "record batches")
-
-	return err
-}
-
 fn main() {
-	srv := flight.NewServerWithMiddleware(nil)
-	srv.Init("0.0.0.0:0")
-	srv.RegisterFlightService(NewServer())
+	let srv = flight.NewServerWithMiddleware(nil);
+	srv.Init("0.0.0.0:0");
+	srv.RegisterFlightService(NewServer());
 	// the Serve function doesn’t return until the server
 	// shuts down. For now we’ll start it running in a goroutine
 	// and shut the server down when our main ends.
 	go srv.Serve()
 	defer srv.Shutdown()
 
-	client, err := flight.NewClientWithMiddleware(srv.Addr().String(), nil, nil, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		panic(err) // handle the error
-	}
+	let client = flight.NewClientWithMiddleware(srv.Addr().String(), nil, nil, grpc.WithTransportCredentials(insecure.NewCredentials())).unwrap();
 	defer client.Close()
 
-	infoStream, err := client.ListFlights(context.TODO(),
-		&flight.Criteria{Expression: []byte("2009")})
-	if err != nil {
-		panic(err) // handle the error
-	}
+	let info_stream = client.ListFlights(context.TODO(),
+		&flight.Criteria{Expression: []byte("2009")}).unwrap();
 
-	for {
-		info, err := infoStream.Recv()
+	loop {
+		let info, err = info_stream.Recv();
 		if err != nil {
 			if err == io.EOF { // we hit the end of the stream
 				break
 			}
 			panic(err) // we got an error!
 		}
-		fmt.Println(info.GetFlightDescriptor().GetPath())
+		println!(info.GetFlightDescriptor().GetPath());
 	}
 
 }
