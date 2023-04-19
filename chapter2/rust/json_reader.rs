f9697d26c1a8d46d7dd0462af8894a40369f64b4
Rin Arakaki <rnarkkx@gmail.com>
2023-04-19 15:42:57 +0900

    Update json_reader.rs

--- chapter2/rust/json_reader.rs
+++ chapter2/rust/json_reader.rs
@@ -2,9 +2,7 @@ use std::fs::File;
 use arrow::json::RawReader;
 
 fn main() {
-    let filename = "sample.json";  // could be any json file
-    let file = File::open(filename).unwrap();
-
+    let file = File::open("sample.json").unwrap();
     let reader = RawReader::new(file).unwrap();
     let table = reader.read().unwrap();
     println!("{}", table);
