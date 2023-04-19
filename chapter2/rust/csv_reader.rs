4a610d5fb3a280d0041e1a448d6d0567b06cb092
Rin Arakaki <rnarkkx@gmail.com>
2023-04-19 15:41:42 +0900

    Update csv_reader.rs

--- chapter2/rust/csv_reader.rs
+++ chapter2/rust/csv_reader.rs
@@ -1,18 +1,12 @@
 use std::fs::File;
 use arrow::{
-    csv::ReaderBuilder,  // the csv functions and objects
-    table   // to read the data into a table
+    csv::ReaderBuilder,
+    table
 };
 
 fn main() {
     let file = File::open("../../sample_data/train.csv").unwrap();
-    let read_options = arrow::csv::ReadOptions::Defaults();
-    let parse_options = arrow::csv::ParseOptions::Defaults();
-    let convert_options = arrow::csv::ConvertOptions::Defaults();
-
-    let reader = Reader::new(file, read_options, parse_options, convert_options).unwrap();
-
-    // finally read the data from the file
+    let reader = ReaderBuilder::new(file).unwrap();
     let table = reader.read().unwrap();
     println!("{}", table);
 }
