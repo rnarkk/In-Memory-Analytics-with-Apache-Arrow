5e112f576a64e7c09ceaa3bd3078ddf94f872f2d
Rin Arakaki <rnarkkx@gmail.com>
2023-04-19 18:00:55 +0900

    Update csv_reader.rs

--- chapter2/rust/csv_reader.rs
+++ chapter2/rust/csv_reader.rs
@@ -1,12 +1,9 @@
 use std::fs::File;
-use arrow::{
-    csv::ReaderBuilder,
-    table
-};
+use arrow::csv::ReaderBuilder;
 
 fn main() {
     let file = File::open("../../sample_data/train.csv").unwrap();
-    let reader = ReaderBuilder::new(file).unwrap();
-    let table = reader.read().unwrap();
-    println!("{}", table);
+    let reader = ReaderBuilder::new().build(file).unwrap();
+    let batch = reader.read().unwrap();
+    println!("{}", batch);
 }
