26444ddb58876ea402c920341d9094dfe16089b9
Rin Arakaki <rnarkkx@gmail.com>
2023-04-19 13:56:52 +0900

    Update examples.rs

--- chapter1/rust/examples.rs
+++ chapter1/rust/examples.rs
@@ -8,25 +8,22 @@ use rand;
 use rand_distr::Normal;
 
 fn first_example() {
-    let data = Vec<i64>![1, 2, 3, 4];
+    let data: Vec<i64> = vec![1, 2, 3, 4];
     let arr = std::make_shared<Int64Array>(data.len(), Buffer::Wrap(data));
     println!("{}", arr);
 }
 
 fn random_data_example() {
-    let rd = std::random_device;
-    let gen = std::mt19937 {rd()};
-    let d = std::normal_distribution<> {5, 2};
-
+    let normal = Normal::new(5, 2).unwrap();
+    let mut rng = rand::thread_rng();
     let builder = arrow::DoubleBuilder {DataType::Float64, pool};
-
     let ncols = 16;
     let nrows = 8192;
     let columns = arrow::ArrayVector (ncols);
     let mut fields = Vec::new();
     for i in 0..ncols {
         for j in 0..nrows {
-            builder.Append(d(gen));
+            builder.append(normal.sample(&mut rng));
         }
         builder.Finish(&columns[i]).unwrap();
         fields.push(Field::new(format!("c{i}"), DataType::Float64, false));
