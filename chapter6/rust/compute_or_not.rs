0740576e70794ed30042a8f554b7ffd342674645
root <rnarkkx@gmail.com>
2023-04-19 16:40:14 +0000

    Update

--- chapter6/rust/compute_or_not.rs
+++ chapter6/rust/compute_or_not.rs
@@ -1,80 +1,66 @@
 use arrow::{
-    array::{Array, ArrayData, Int32Builder},
+    array::{Int32Array, Int32Builder},
     buffer::Buffer,
-    compute as cp,
-    data
+    compute::*,
 };
 use super::timer;
 
 fn main() {
     for n in (10000..=10000000).skip(10000) {
-        let testvalues = Vec<i32>(n);
-        std::iota(std::begin(testvalues), std::end(testvalues), 0);
-
-        let nb = Int32Builder::new();
-        nb.append_values(testvalues);
-        let numarr = nb.finish();
-
         println!("N: {}", n);
+        let values: Vec<i32> = (0..n).collect();
+        let arr = Int32Array::from(values);
 
-        let arr = std::static_pointer_cast<arrow::Int32Array>(numarr);
-
-        let res1 = arrow::Datum;
-        {
+        let res1 = {
             let t = timer;
-            res1 = cp::add(arr, arrow::Datum{2i32}).MoveValueUnsafe();
-        }
+            add_scalar(&arr, 2i32)
+        };
 
-        let res2 = arrow::Datum;
-        {
+        let res2 = {
             let t = timer;
             let b = Int32Builder::new();
             for i in 0..arr.len() {
-                if arr.IsValid(i) {
+                if arr.is_valid(i) {
                     b.append(arr.value(i) + 2);
                 } else {
                     b.append_null();
                 }
             }
-            let output = b.finish();
-            res2 = arrow::Datum{std::move(output)};
-        }
+            b.finish()
+        };
         println!("{}", res1 == res2);
 
-        let res3 = arrow::Datum;
+        let res3 =
         {
             let t = timer;
             let arr = std::static_pointer_cast<arrow::Int32Array>(numarr);
             let b = Int32Builder::with_capacity(arr.len());
-            std::for_each(std::begin(*arr), std::end(*arr),
-                [&b]|v: const arrow::util::optional<i32>&| {
+            arr.for_each(|v: &arrow::util::optional<i32>| {
                     if v {
                         b.append(*v + 2);
                     } else {
                         b.append_null();
                     }
                 });
-            let output = b.finish();
-            res3 = arrow::Datum{std::move(output)};
-        }
+            b.finish()
+        };
         println!("{}", res1 == res3);
 
-        let res4 = arrow::Datum;
+        let res4 =
         {
             let t = timer;
             let arr = std::static_pointer_cast<arrow::Int32Array>(numarr);
-            let newbuf = std::shared_ptr<Buffer> =
-                arrow::AllocateBuffer(sizeof(i32) * arr.len())
+            let newbuf = arrow::AllocateBuffer(sizeof(i32) * arr.len())
                     .MoveValueUnsafe();
             let output = reinterpret_cast<*i32>(newbuf.mutable_data());
             std::transform(arr.raw_values(), arr.raw_values() + arr.len(),
                             output, |v: i32| v + 2);
 
-            res4 = arrow::Datum{arrow::MakeArray(
+            arrow::MakeArray(
                 ArrayData::new(arr.r#type(), arr.len(),
                     Vec<std::shared_ptr<Buffer>>{arr.null_bitmap(), newbuf},
-                    arr.null_count()))};
-        }
+                    arr.null_count()))
+        };
         println!("{}", res1 == res4);
     }
 }
