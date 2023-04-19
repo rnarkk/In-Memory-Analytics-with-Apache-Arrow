f59ab825ed68747e1bb7fac9710816eadeedd60d
Rin Arakaki <rnarkkx@gmail.com>
2023-04-19 15:14:40 +0900

    Update todo.rs

--- chapter1/rust/todo.rs
+++ chapter1/rust/todo.rs
@@ -1,7 +1,4 @@
 fn building_struct_array() {
-  using arrow::field;
-  using arrow::int16;
-  using arrow::utf8;
     let mut children = Vec::with_capacity(3);
 
     let archers = vec!["Legolas", "Oliver", "Merida", "Lara", "Artemis"];
@@ -28,42 +25,41 @@ fn building_struct_array() {
 
 fn build_struct_builder() {
     std::shared_ptr<arrow::DataType> st_type = arrow::struct_(
-      {field("archer", arrow::utf8()), field("location", arrow::utf8()),
-       field("year", arrow::int16())});
+      vec![Field::new("archer", DataType::Utf8),
+      Field::new("location", DataType::Utf8),
+       Field::new("year", DataType::Int16)]);
 
   std::unique_ptr<arrow::ArrayBuilder> tmp;
   arrow::MakeBuilder(arrow::default_memory_pool(), st_type, &tmp);
   std::shared_ptr<arrow::StructBuilder> builder;
   builder.reset(static_cast<arrow::StructBuilder*>(tmp.release()));
 
-  using namespace arrow;
   StringBuilder* archer_builder =
-      static_cast<StringBuilder*>(builder->field_builder(0));
+      static_cast<StringBuilder*>(builder.field_builder(0));
   StringBuilder* location_builder =
-      static_cast<StringBuilder*>(builder->field_builder(1));
+      static_cast<StringBuilder*>(builder.field_builder(1));
   Int16Builder* year_builder =
-      static_cast<Int16Builder*>(builder->field_builder(2));
+      static_cast<Int16Builder*>(builder.field_builder(2));
 
-  std::vector<std::string> archers{"Legolas", "Oliver", "Merida", "Lara",
+    let archers = vec!["Legolas", "Oliver", "Merida", "Lara",
                                    "Artemis"};
-  std::vector<std::string> locations{"Murkwood", "Star City", "Scotland",
+    let locations = vec!["Murkwood", "Star City", "Scotland",
                                      "London", "Greece"};
-  std::vector<int16_t> years{1954, 1941, 2012, 1996, -600};
+  std::vector<int16_t> years = vec![1954, 1941, 2012, 1996, -600};
 
-  for (int i = 0; i < archers.size(); ++i) {
-    builder->Append();
-    archer_builder->Append(archers[i]);
-    location_builder->Append(locations[i]);
-    year_builder->Append(years[i]);
-  }
+    for i in 0..archers.len() {
+        builder.Append();
+        archer_builder.Append(archers[i]);
+        location_builder.Append(locations[i]);
+        year_builder.Append(years[i]);
+    }
 
-  std::shared_ptr<arrow::Array> out;
-  builder->Finish(&out);
-  std::cout << out->ToString() << std::endl;
+    let out = builder.finish();
+    println!("{}", out);
 }
 
 fn main() {
-  first_example();
+    first_example();
   random_data_example();
   building_struct_array();
   build_struct_builder();
