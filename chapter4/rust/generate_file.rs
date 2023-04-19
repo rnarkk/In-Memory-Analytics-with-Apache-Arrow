74b6a235d2be73f1f5f595607068f29563cb7889
Rin Arakaki <rnarkkx@gmail.com>
2023-04-19 18:16:05 +0900

    Update generate_file.rs

--- chapter4/rust/generate_file.rs
+++ chapter4/rust/generate_file.rs
@@ -11,19 +11,18 @@ use parquet as pq;
 
 fn main() {
     let file = File::open("yellow_tripdata_2015-01.csv").unwrap();
-    let build = ReaderBuilder::new().infer_schema(Some(100));
-    let reader = builder.build(file).unwrap();
-    let tbl = pa.csv.read_csv();
-    pq.write_table(tbl, "yellow_tripdata_2015-01.parquet");
+    let reader = ReaderBuilder::new().infer_schema(Some(100)).build(file).unwrap();
+    let batch = reader.next();
+    pq.write_table(batch, "yellow_tripdata_2015-01.parquet");
     let sink = pa.OSFile("yellow_tripdata_2015-01.arrow", "wb");
-    let writer = pa.RecordBatchFileWriter(sink, tbl.schema);
-    writer.write_table(tbl);
+    let writer = pa.RecordBatchFileWriter(sink, batch.schema);
+    writer.write_table(batch);
 
     // fill out the NaN values with 0s so that we can zero-copy it to
     // a pandas dataframe in our memory usage test
-    let df = tbl.to_pandas().fillna(0);
-    let tbl = pa.Table.from_pandas(df);
+    let df = batch.to_pandas().fillna(0);
+    let batch = pa.Table.from_pandas(df);
     let sink = pa.OSFile("yellow_tripdata_2015-01-nonan.arrow", "wb");
-    let writer = pa.RecordBatchFileWriter(sink, tbl.schema);
-    writer.write_table(tbl);
+    let writer = pa.RecordBatchFileWriter(sink, batch.schema);
+    writer.write_table(batch);
 }
